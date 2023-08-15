ace0c769d28fad4489ca615fb8110b25a2d98876
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2023-08-15 17:43:05 -0300

    Atualização de usuário com PUT

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -38,6 +38,24 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+	    Optional<User> optionalUser = repo.findById(obj.getId());
+	    
+	    if (optionalUser.isPresent()) {
+	        User newObj = optionalUser.get();
+	        updateData(newObj, obj);
+	        return repo.save(newObj);
+	    } else {
+	        // Handle the case where the user with the given ID is not found.
+	        throw new RuntimeException("User not found with ID: " + obj.getId());
+	    }
+	}
+
+	private void updateData(User newObj, User obj) {
+	    newObj.setName(obj.getName());
+	    newObj.setEmail(obj.getEmail());
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
