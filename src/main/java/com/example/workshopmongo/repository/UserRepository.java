e6cd6d6d76d1abb92fd4baca6a1262aae5c7f5bd
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2023-08-15 00:19:57 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/example/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/example/workshopmongo/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.User;
-
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
