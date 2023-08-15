d9a687690f919908a4c32cf3f31264c823263d9d
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2023-08-14 21:45:37 -0300

    Entity User e REST funcionando

--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,12 @@ import com.example.workshopmongo.domain.User;
 public class UserResource {
 	
 	@GetMapping
-	public List<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		User maria = new User("1", "Maria Brawn", "maria@gmail.com");
 		User alex = new User("2","Alex Green","alex@gmail.com");
 		List<User> list = new ArrayList<>();
 		list.addAll(Arrays.asList(maria,alex));
-		return list;
+		return ResponseEntity.ok().body(list);
 		
 	}
 }
