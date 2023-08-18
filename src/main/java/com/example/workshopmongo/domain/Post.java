a36c3c09d2d50283a90ad3fbdd04537957f4b7ef
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2023-08-18 20:28:57 -0300

    Consulta com vários critérios

--- src/main/java/com/example/workshopmongo/domain/Post.java
+++ src/main/java/com/example/workshopmongo/domain/Post.java
@@ -18,7 +18,7 @@ public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	private String id;
-	private Date data;
+	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
@@ -28,10 +28,10 @@ public class Post implements Serializable{
 	public Post() {
 	}
 
-	public Post(String id, Date data, String title, String body, AuthorDTO author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
-		this.data = data;
+		this.date = date;
 		this.title = title;
 		this.body = body;
 		this.author = author;
@@ -45,12 +45,12 @@ public class Post implements Serializable{
 		this.id = id;
 	}
 
-	public Date getData() {
-		return data;
+	public Date getDate() {
+		return date;
 	}
 
-	public void setData(Date data) {
-		this.data = data;
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
 	public String getTitle() {
--- src/main/java/com/example/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.resources.util.URL;
 import com.example.workshopmongo.services.PostService;
 
 @RestController
@@ -26,9 +28,22 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);	
 	}
 	
-	@GetMapping(value = "/titleseach")
+	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text){
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);	
+	}
+	
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text",defaultValue = "") String text,
+			@RequestParam(value = "minDate" , defaultValue ="")String minDate,
+			@RequestParam(value = "maxDate" , defaultValue ="")String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text,min,max);
 		return ResponseEntity.ok().body(list);	
 	}
 	
